bdc55cf9833cbc927af24cb76c873fba53d8ee86
sidekick <runner@fv-az1102-724.ghvsf53df5vutddeto4bu05etf.jx.internal.cloudapp.net>
2023-06-02 15:21:09 +0000

    Deploy

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,28 +1,9 @@
-"use client";
-
-import Image from 'next/image'
-import ChatBot from 'react-simple-chatbot';
- 
-const steps = [
-  {
-    id: '0',
-    message: 'Welcome to react chatbot!',
-    trigger: '1',
-  },
-  {
-    id: '1',
-    message: 'Bye!',
-    end: true,
-  },
-];
-
-export default function Home() {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div>
-    oui
-    </div>
-    <ChatBot steps={steps} floating={true} className={"bg-orange-200"} />
+      <div>
+        oui
+      </div>
     </main>
   )
 }
