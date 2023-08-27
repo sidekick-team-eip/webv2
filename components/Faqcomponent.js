a6d99901d6c0fd659a651bfc0342e6d02243a0ac
sidekick <runner@fv-az621-295.hgz2n3xr2b2uteg1gmg5rpevog.jx.internal.cloudapp.net>
2023-08-27 09:19:38 +0000

    Deploy

--- components/Faqcomponent.js
+++ components/Faqcomponent.js
@@ -1,76 +1,76 @@
 export default function Faqcomponent() {
   return (
-    <section class="text-gray-600 body-font">
+    <section className="text-gray-600 body-font">
 
       <div className="pt-12 max-w-5xl mx-auto md:px-1 px-3">
-        <div class="ktq4 text-center">
-          <h3 class="pt-3 font-semibold text-lg text-white">
+        <div className="ktq4 text-center">
+          <h3 className="pt-3 font-semibold text-lg text-white">
             La Foire Au questions
           </h3>
-          <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-          Bienvenue sur la page FAQ de Sidekick ! Vous trouverez ici des réponses a vos questions concernant notre application. N'hésitez pas à explorer les questions et réponses ci-dessous pour obtenir une meilleure compréhension de notre projet et de la manière dont il peut vous aider à atteindre vos objectifs sportifs et nutritionnels!
+          <p className="pt-2 value-text text-md text-gray-200 fkrr1">
+            Bienvenue sur la page FAQ de Sidekick ! Vous trouverez ici des réponses a vos questions concernant notre application. N'hésitez pas à explorer les questions et réponses ci-dessous pour obtenir une meilleure compréhension de notre projet et de la manière dont il peut vous aider à atteindre vos objectifs sportifs et nutritionnels!
 
           </p>
         </div>
       </div>
 
       <div className="pt-12 max-w-6xl mx-auto fsac4 md:px-1 px-3">
-        <div class="ktq5">
-          <h1 class="font-bold text-white">1</h1>
-          <h3 class="pt-3 font-semibold text-title-faq text-white">
+        <div className="ktq5">
+          <h1 className="font-bold text-white">1</h1>
+          <h3 className="pt-3 font-semibold text-title-faq text-white">
             Qu'est-ce que Sidekick et en quoi consiste ce projet ?
           </h3>
-          <p class="pt-2 value-text text-faq text-gray-200 fkrr1">
+          <p className="pt-2 value-text text-faq text-gray-200 fkrr1">
             Sidekick est une application qui met en relation deux personnes inconnues afin qu'elles puissent s'entraider pour atteindre leurs objectifs communs, que ce soit dans le domaine du sport et/ou d'un plan alimentaire.
           </p>
         </div>
 
-        <div class="ktq5">
-          <h1 class="font-bold text-white">2</h1>
-          <h3 class="pt-3 font-semibold text-title-faq text-white">
+        <div className="ktq5">
+          <h1 className="font-bold text-white">2</h1>
+          <h3 className="pt-3 font-semibold text-title-faq text-white">
             Comment fonctionne Sidekick ?
           </h3>
-          <p class="pt-2 value-text text-faq text-gray-200 fkrr1">
+          <p className="pt-2 value-text text-faq text-gray-200 fkrr1">
             Sidekick permet aux utilisateurs de se connecter en fonction de leurs intérêts sportifs et de leurs objectifs. L'application propose des binômes de partenaires qui s'encouragent mutuellement à atteindre leurs buts.
           </p>
         </div>
 
-        <div class="ktq5">
-          <h1 class="font-bold text-white">3</h1>
-          <h3 class="pt-3 font-semibold text-title-faq text-white">
+        <div className="ktq5">
+          <h1 className="font-bold text-white">3</h1>
+          <h3 className="pt-3 font-semibold text-title-faq text-white">
             Quels sont les avantages de l'utilisation de Sidekick ?
           </h3>
-          <p class="pt-2 value-text text-faq text-gray-200 fkrr1">
+          <p className="pt-2 value-text text-faq text-gray-200 fkrr1">
             Sidekick offre une approche collaborative pour rester motivé dans vos activités sportives. Vous pouvez trouver un partenaire qui partage vos objectifs et vous soutient tout au long de votre parcours.
           </p>
         </div>
 
-        <div class="ktq5">
-          <h1 class="font-bold text-white">4</h1>
-          <h3 class="pt-3 font-semibold text-title-faq text-white">
+        <div className="ktq5">
+          <h1 className="font-bold text-white">4</h1>
+          <h3 className="pt-3 font-semibold text-title-faq text-white">
             Comment contacter le support Sidekick ?
           </h3>
-          <p class="pt-2 value-text text-faq text-gray-200 fkrr1">
+          <p className="pt-2 value-text text-faq text-gray-200 fkrr1">
             Si vous avez des questions, vous pouvez nous envoyer un e-mail à sidekick.eip@gmail.com. Nous serons ravis de répondre à vos interrogations ou vos retours et de vous aider dans votre expérience avec Sidekick.
           </p>
         </div>
 
-        <div class="ktq5">
-          <h1 class="font-bold text-white">5</h1>
-          <h3 class="pt-3 font-semibold text-title-faq text-white">
+        <div className="ktq5">
+          <h1 className="font-bold text-white">5</h1>
+          <h3 className="pt-3 font-semibold text-title-faq text-white">
             Comment puis-je télécharger l'application Sidekick ?
           </h3>
-          <p class="pt-2 value-text text-faq text-gray-200 fkrr1">
+          <p className="pt-2 value-text text-faq text-gray-200 fkrr1">
             Vous pouvez télécharger l'application Sidekick en cliquant sur les liens de téléchargement pour Android sur notre site Web. La version iOS devrais arriver sur les plateformes prochainement!
           </p>
         </div>
 
-        <div class="ktq5">
-          <h1 class="font-bold text-white">6</h1>
-          <h3 class="pt-3 font-semibold text-title-faq text-white">
+        <div className="ktq5">
+          <h1 className="font-bold text-white">6</h1>
+          <h3 className="pt-3 font-semibold text-title-faq text-white">
             L'application Sidekick est-elle gratuite à utiliser ?
           </h3>
-          <p class="pt-2 value-text text-faq text-gray-200 fkrr1">
+          <p className="pt-2 value-text text-faq text-gray-200 fkrr1">
             Oui, l'application Sidekick est entièrement gratuite à télécharger et à utiliser. Nous croyons en l'accessibilité à tous pour atteindre ses objectifs !
           </p>
         </div>
@@ -79,10 +79,10 @@ export default function Faqcomponent() {
 
 
 
-      <section class="relative">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 text-center">
-          <p class="pt-6 text-gray-200">
-            Ou envoyer nous un email : <b class="text-orange-500">sidekick.eip@gmail.com</b>
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
+          <p className="pt-6 text-gray-200">
+            Ou envoyer nous un email : <b className="text-orange-500">sidekick.eip@gmail.com</b>
           </p>
         </div>
       </section>
--- components/Main.js
+++ components/Main.js
@@ -1,134 +1,134 @@
 export default function Main() {
-    return (
-      <section class="text-gray-600 body-font">
-        
-        <div class="max-w-5xl pt-20 pb-36 mx-auto">
-          <h1 class="text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6 pb-10">
+  return (
+    <section className="text-gray-600 body-font">
+
+      <div className="max-w-5xl pt-20 pb-36 mx-auto">
+        <h1 className="text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6 pb-10">
           Ensemble, allons plus loin !
-          </h1>
-          <h2 class="text-2xl font-5 lh-6 ld-04 pb-11 text-slate-300 text-center">
+        </h1>
+        <h2 className="text-2xl font-5 lh-6 ld-04 pb-11 text-slate-300 text-center">
           Sidekick c’est la fusion d’une application de nutrition et d’exercices sportifs complets avec un espace de rencontre entre passionnés pour rester motivé.
-          </h2>
-          <div class="text-center">
+        </h2>
+        <div className="text-center">
           <a
-            class="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-black transition duration-500 ease-in-out transform bg-gradient-to-r from-orange-500 to-orange-700 hover:from-pink-500 border rounded-lg"
+            className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-black transition duration-500 ease-in-out transform bg-gradient-to-r from-orange-500 to-orange-700 hover:from-pink-500 border rounded-lg"
             href="/app-release.apk"
           >
-            <span class="justify-center">Get for Android</span>
+            <span className="justify-center">Get for Android</span>
           </a>
           <a
-            class="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-black transition duration-500 ease-in-out transform bg-gray border rounded-lg"
+            className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-black transition duration-500 ease-in-out transform bg-gray border rounded-lg"
             href="/"
           >
-            <span class="justify-center disabled">Get for iOS</span>
+            <span className="justify-center disabled">Get for iOS</span>
           </a>
-          </div>
         </div>
+      </div>
 
-        <div className="pt-12 pb-12 max-w-5xl mx-auto md:px-1 px-3 text-center">
-          <div class="ktq4">
-            <img class="h-20 w-20" src="../logo.png"></img>
-            <h3 class="pt-3 font-semibold text-lg text-white">
+      <div className="pt-12 pb-12 max-w-5xl mx-auto md:px-1 px-3 text-center">
+        <div className="ktq4">
+          <img className="h-20 w-20" src="../logo.png"></img>
+          <h3 className="pt-3 font-semibold text-lg text-white">
             Decouvrir le projet
-            </h3>
-            <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-              Sidekick est une application qui met en relation deux inconnus pour qu'ils puissent s'entraider et atteindre leurs objectifs communs, que ce soit pour le sport et/ou pour un plan alimentaire.
-              Le but est d'améliorer la vie de nos utilisateurs, en leur facilitant l'accés aux sports et aux programmes sportifs, et ainsi d'améliorer leur santé.
-              Avec nous trouvez votre acolyte pour votre aventure sportive !
-            </p>
+          </h3>
+          <p className="pt-2 value-text text-md text-gray-200 fkrr1">
+            Sidekick est une application qui met en relation deux inconnus pour qu'ils puissent s'entraider et atteindre leurs objectifs communs, que ce soit pour le sport et/ou pour un plan alimentaire.
+            Le but est d'améliorer la vie de nos utilisateurs, en leur facilitant l'accés aux sports et aux programmes sportifs, et ainsi d'améliorer leur santé.
+            Avec nous trouvez votre acolyte pour votre aventure sportive !
+          </p>
+        </div>
+      </div>
+
+      <div className=" max-w-5xl mx-auto md:px-1 px-3">
+        <div className="ktq4 text-center">
+          <p className="pb-8 mx-auto text-xl text-center text-gray-300 font-semibold leading-relaxed fs521 lg:w-2/3">
+            La timeline du projet<br></br>
+          </p>
+          <div className="container flex flex-col items-center justify-center mx-auto">
+            <img
+              className="object-cover object-center w-3/4 mb-10 border shadow-md g327"
+              alt="Timeline Image"
+              src="../timetable.png"
+            ></img>
           </div>
         </div>
-
-        <div className=" max-w-5xl mx-auto md:px-1 px-3">
-        <div class="ktq4 text-center">
-        <p className="pb-8 mx-auto text-xl text-center text-gray-300 font-semibold leading-relaxed fs521 lg:w-2/3">
-          La timeline du projet<br></br>
-        </p>
-        <div className="container flex flex-col items-center justify-center mx-auto">
-          <img
-            className="object-cover object-center w-3/4 mb-10 border shadow-md g327"
-            alt="Timeline Image"
-            src="../timetable.png"
-          ></img>
-        </div>
-        </div>
-        </div>
+      </div>
 
 
-        <div className="pt-12 max-w-5xl mx-auto md:px-1 px-3">
-          <div class="ktq4 text-center">
-            <h3 class="pt-3 font-semibold text-lg text-white">
+      <div className="pt-12 max-w-5xl mx-auto md:px-1 px-3">
+        <div className="ktq4 text-center">
+          <h3 className="pt-3 font-semibold text-lg text-white">
             L'equipe
-            </h3>
-            <p class="pt-2 value-text text-md text-gray-200 fkrr1">
+          </h3>
+          <p className="pt-2 value-text text-md text-gray-200 fkrr1">
             Nous sommes une équipe d'étudiants français en informatique, passionnés par les nouvelles technologies,
-             mais aussi, comme vous vous en doutez, par le sport ! Nous avons décidé de fonder Sidekick,
-              notre propre entreprise afin d'allier nos passions.
-             Pour le moment, nous sommes partout dans le monde, mais nous travaillons tous ensemble sur notre projet !
+            mais aussi, comme vous vous en doutez, par le sport ! Nous avons décidé de fonder Sidekick,
+            notre propre entreprise afin d'allier nos passions.
+            Pour le moment, nous sommes partout dans le monde, mais nous travaillons tous ensemble sur notre projet !
+          </p>
+        </div>
+      </div>
+
+      <div className="pt-12 max-w-4xl mx-auto fsac4 md:px-1 px-3">
+        <div className="ktq4">
+          <img className="w-10" src="../Alizee.png"></img>
+          <h3 className="pt-3 font-semibold text-lg text-white">
+            Alizee S
+          </h3>
+          <p className="pt-2 value-text text-md text-gray-200 fkrr1">
+            Je suis la touche graphique du groupe.
+          </p>
+        </div>
+        <div className="ktq4">
+          <img className="w-10" src="../Gregoire.png"></img>
+          <h3 className="pt-3 font-semibold text-lg text-white">
+            Gregoire D
+          </h3>
+          <p className="pt-2 value-text text-md text-gray-200 fkrr1">
+            Je connais mieux la salle de sport que mon appartement, je suis le coatch sportif du groupe.
+          </p>
+        </div>
+        <div className="ktq4">
+          <img className="w-10" src="../Alex.png"></img>
+          <h3 className="pt-3 font-semibold text-lg text-white">
+            Alex A
+          </h3>
+          <p className="pt-2 value-text text-md text-gray-200 fkrr1">
+            Entre deux randonnées dans les Fjords, je suis le developpeur full stack de l'equipe.
+          </p>
+        </div>
+        <div className="ktq4">
+          <img className="w-10" src="../Theo.png"></img>
+          <h3 className="pt-3 font-semibold text-lg text-white">
+            Theo P
+          </h3>
+          <p className="pt-2 value-text text-md text-gray-200 fkrr1">
+            Tout ce que vous ne voyez pas, c'est moi qui l'ai construit. Mon nom est monsieur Back End.
+          </p>
+        </div>
+      </div>
+
+
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
+          <div className="py-24 md:py-36">
+            <h1 className="mb-9 text-2xl font-semibold text-gray-200">
+              Des questions sur le projet? La page FAQ est la pour vous !
+            </h1>
+            <a
+              className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-black transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-white"
+              href="/faq"
+            >
+              <span className="justify-center">FAQ</span>
+            </a>
+
+            <p className="pt-6 text-gray-200">
+              Ou envoyer nous un email : <b className="text-orange-500">sidekick.eip@gmail.com</b>
             </p>
+
           </div>
         </div>
-
-        <div className="pt-12 max-w-4xl mx-auto fsac4 md:px-1 px-3">
-          <div class="ktq4">
-            <img className="w-10" src="../Alizee.png"></img>
-            <h3 class="pt-3 font-semibold text-lg text-white">
-              Alizee S
-            </h3>
-            <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-              Je suis la touche graphique du groupe.
-            </p>
-          </div>
-          <div class="ktq4">
-            <img className="w-10" src="../Gregoire.png"></img>
-            <h3 class="pt-3 font-semibold text-lg text-white">
-              Gregoire D
-            </h3>
-            <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-              Je connais mieux la salle de sport que mon appartement, je suis le coatch sportif du groupe.
-            </p>
-          </div>
-          <div class="ktq4">
-            <img className="w-10" src="../Alex.png"></img>
-            <h3 class="pt-3 font-semibold text-lg text-white">
-              Alex A
-            </h3>
-            <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-              Entre deux randonnées dans les Fjords, je suis le developpeur full stack de l'equipe.
-            </p>
-          </div>
-          <div class="ktq4">
-            <img className="w-10" src="../Theo.png"></img>
-            <h3 class="pt-3 font-semibold text-lg text-white">
-              Theo P
-            </h3>
-            <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-              Tout ce que vous ne voyez pas, c'est moi qui l'ai construit. Mon nom est monsieur Back End.
-            </p>
-          </div>
-        </div>
-
-
-        <section class="relative">
-          <div class="max-w-6xl mx-auto px-4 sm:px-6 text-center">
-            <div class="py-24 md:py-36">
-              <h1 class="mb-9 text-2xl font-semibold text-gray-200">
-                Des questions sur le projet? La page FAQ est la pour vous !
-              </h1>
-              <a
-                class="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-black transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-white"
-                href="/faq"
-              >
-                <span class="justify-center">FAQ</span>
-              </a>
-
-              <p class="pt-6 text-gray-200">
-                 Ou envoyer nous un email : <b class="text-orange-500">sidekick.eip@gmail.com</b>
-              </p>
-
-            </div>
-          </div>
-        </section>
       </section>
-    );
-  }
+    </section>
+  );
+}
