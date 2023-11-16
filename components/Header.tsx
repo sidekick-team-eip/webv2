96015da75b5b75a845843f94241d22b3edd67dc1
sidekick <sidekick.eip@gmail.com>
2023-11-16 09:36:53 +0000

    Deploy

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,154 +9,162 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import MenuItem from '@mui/material/MenuItem';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { Button } from "@mui/material";
+import {Button} from "@mui/material";
 import Link from "next/link";
 import Image from 'next/image';
 // @ts-ignore
-import { signOut, useSession } from 'next-auth/react';
+import {signOut, useSession} from 'next-auth/react';
 
 const pages = [{
-  label: 'Login',
-  href: '/signin'
+    label: 'Reset password',
+    href: '/reset_password'
 }, {
-  label: 'Signup',
-  href: '/signup'
+    label: 'Login',
+    href: '/signin'
+}, {
+    label: 'Signup',
+    href: '/signup'
 }, {
     label: 'Beta',
     href: '/beta',
 }];
 
 const pagesAuth = [{
-  label: 'Profile',
-  href: '/profile',
+    label: 'Profile',
+    href: '/profile',
 }, {
-  label: 'Planning',
-  href: '/planning',
+    label: 'Planning',
+    href: '/planning',
 }, {
-  label: 'Tips',
-  href: '/tips',
+    label: 'Tips',
+    href: '/tips',
 }, {
-  label: 'Chat',
-  href: '/chat',
+    label: 'Chat',
+    href: '/chat',
 }, {
-  label: 'Logout',
-  href: '#',
-  onClick: async () => signOut()
+    label: 'Logout',
+    href: '#',
+    onClick: async () => signOut()
 }];
 
 function Header() {
-  const { data: session } = useSession();
-  //console.log("header", session);
-  const mobile = useMediaQuery('(max-width:639px)');
+    const {data: session} = useSession();
+    //console.log("header", session);
+    const mobile = useMediaQuery('(max-width:639px)');
 
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  return (
-    <AppBar position="relative" color="transparent" elevation={0} className='p-4'>
-      <Container maxWidth="lg">
-        <Toolbar disableGutters className='flex justify-between'>
-          <Box>
-            <Link href="/">
-              <Image src="/logo_transparent.png" alt="Sidekick" width={32} height={32} />
-            </Link>
-          </Box>
+    return (
+        <AppBar position="relative" color="transparent" elevation={0} className='p-4'>
+            <Container maxWidth="lg">
+                <Toolbar disableGutters className='flex justify-between'>
+                    <Box>
+                        <Link href="/">
+                            <Image src="/logo_transparent.png" alt="Sidekick" width={32} height={32}/>
+                        </Link>
+                    </Box>
 
-          {mobile ? (
-            <Box sx={{ flexGrow: 0 }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: 'block', md: 'none' },
-                }}
-              >
-                {session ?
-                  <>
-                    {pagesAuth.map((page) => (
-                      <Link href={page.href} key={page.href} onClick={page.onClick ?? undefined}>
-                        <MenuItem onClick={handleCloseNavMenu}>
-                          <Typography textAlign="center">{page.label}</Typography>
-                        </MenuItem>
-                      </Link>
-                    ))}
-                  </>
-                  :
-                  <>
-                    {pages.map((page) => (
-                      <Link href={page.href} key={page.href}>
-                        <MenuItem onClick={handleCloseNavMenu}>
-                          <Typography textAlign="center">{page.label}</Typography>
-                        </MenuItem>
-                      </Link>
-                    ))}
-                  </>
-                }
-              </Menu>
-            </Box >
-          ) : (
-            session ? (
-              <Box className="space-x-8" sx={{ flexGrow: 0 }} >
-                {pagesAuth.map((page) => (
-                  <Link href={page.href} key={page.label} onClick={page.onClick ?? undefined}>
-                    <Button variant="outlined">
-                      {page.label}
-                    </Button>
-                  </Link>
-                ))}
-              </Box>
-            ) : (
-              <Box className="space-x-8" sx={{ flexGrow: 0 }} >
-                <Link href="/beta">
-                  <Button variant="text" color="primary">
-                    Beta
-                  </Button>
-                </Link>
-                <Link href="/signin">
-                  <Button variant="text" color="primary">
-                    Log in
-                  </Button>
-                </Link>
-                <Link href="/signup">
-                  <Button variant="contained" className="bg-orangePrimary">
-                    Sign up
-                  </Button>
-                </Link>
-              </Box>
-            )
-          )}
-        </Toolbar >
-      </Container >
-    </AppBar >
-  );
+                    {mobile ? (
+                        <Box sx={{flexGrow: 0}}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon/>
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: {xs: 'block', md: 'none'},
+                                }}
+                            >
+                                {session ?
+                                    <>
+                                        {pagesAuth.map((page) => (
+                                            <Link href={page.href} key={page.href} onClick={page.onClick ?? undefined}>
+                                                <MenuItem onClick={handleCloseNavMenu}>
+                                                    <Typography textAlign="center">{page.label}</Typography>
+                                                </MenuItem>
+                                            </Link>
+                                        ))}
+                                    </>
+                                    :
+                                    <>
+                                        {pages.map((page) => (
+                                            <Link href={page.href} key={page.href}>
+                                                <MenuItem onClick={handleCloseNavMenu}>
+                                                    <Typography textAlign="center">{page.label}</Typography>
+                                                </MenuItem>
+                                            </Link>
+                                        ))}
+                                    </>
+                                }
+                            </Menu>
+                        </Box>
+                    ) : (
+                        session ? (
+                            <Box className="space-x-8" sx={{flexGrow: 0}}>
+                                {pagesAuth.map((page) => (
+                                    <Link href={page.href} key={page.label} onClick={page.onClick ?? undefined}>
+                                        <Button variant="outlined">
+                                            {page.label}
+                                        </Button>
+                                    </Link>
+                                ))}
+                            </Box>
+                        ) : (
+                            <Box className="space-x-8" sx={{flexGrow: 0}}>
+                                <Link href="/reset_password">
+                                    <Button variant="text" color="primary">
+                                        Reset password
+                                    </Button>
+                                </Link>
+                                <Link href="/beta">
+                                    <Button variant="text" color="primary">
+                                        Beta
+                                    </Button>
+                                </Link>
+                                <Link href="/signin">
+                                    <Button variant="text" color="primary">
+                                        Log in
+                                    </Button>
+                                </Link>
+                                <Link href="/signup">
+                                    <Button variant="contained" className="bg-orangePrimary">
+                                        Sign up
+                                    </Button>
+                                </Link>
+                            </Box>
+                        )
+                    )}
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
 
 export default Header;
--- pages/reset_password/index.tsx
+++ pages/reset_password/index.tsx
@@ -1,63 +1,72 @@
-import { ConfirmationNumberOutlined } from '@mui/icons-material';
-import { Button } from '@mui/material';
+import {ConfirmationNumberOutlined} from '@mui/icons-material';
+import {Button} from '@mui/material';
 import TextField from '@mui/material/TextField';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from "react";
+import {useRouter} from 'next/router';
+import React, {useState} from "react";
+import axios from "axios";
 
 async function resetPassword(email: string, password: string, confirmPassword: string, code: string) {
-	if (password !== confirmPassword) {
-	  return false;
-	}
+    if (password !== confirmPassword) {
+        return false;
+    }
 
-	try {
-	  var requestOptions = {
-		method: 'POST',
-		headers: { "Content-Type": 'application/json' },
-		body: JSON.stringify({
-		  email: email,
-		  password: password,
-		  verificationCode: code,
-		})
-	  };
+    try {
+        /* const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/resetPassword`, JSON.stringify({
+            email: email,
+            password: password,
+            verificationCode: code,
+        }), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        }); */
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/resetPassword`, {
+            email: email,
+            password: password,
+            verificationCode: code,
+        })
 
-	  return await fetch(process.env.NEXT_PUBLIC_API_URL + "/auth/resetPassword/", requestOptions)
-		.then(response => {
-			return true;
-		})
-		.catch(error => {
-		  console.log('Error: ', error);
-		});
-	}
-	catch (error) {
-	  console.log('Invalid code : ', error);
-	  return false
-	}
+        return true
+    } catch (error) {
+        console.log('Invalid code : ', error);
+        return false
+    }
 }
 
 export default function Planning() {
-	const [password, setPassword] = useState("");
-	const [code, setCode] = useState("");
-	const [email, setEmail] = useState("");
-	const [confirmPassword, setConfirmPassword] = useState("");
-	const router = useRouter();
-  
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+    const [password, setPassword] = useState("");
+    const [code, setCode] = useState("");
+    const [email, setEmail] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const router = useRouter();
 
-		if (await resetPassword(email, password, confirmPassword, code) === true) {
-			router.push("/signin")
-		}
-	  }
-  
-	return (
-		<div className='flex items-center justify-center'>
-			<form className='flex flex-col space-y-4' onSubmit={handleSubmit}>
-				<TextField placeholder="123456" focused color="white" InputProps={{  style: { fontStyle:'italic', color: 'grey'},}} className="w-full" name='code' label="Code" variant="outlined" value={code} onChange={e => setCode(e.target.value)} />
-				<TextField placeholder="exemple@gmail.com" focused color="white" InputProps={{  style: { fontStyle:'italic', color: 'grey'},}} className="w-full" name='email' label="Email" variant="outlined" value={email} onChange={e => setEmail(e.target.value)} />
-				<TextField placeholder="*******" focused color="white" InputProps={{  style: { fontStyle:'italic', color: 'grey'},}} className="w-full" name='password' type='password' label="Password" variant="outlined" value={password} onChange={e => setPassword(e.target.value)} />
-				<TextField placeholder="*******" focused color="white" InputProps={{  style: { fontStyle:'italic', color: 'grey'},}} className="w-full" name='confirmPassword' type='password' label="confirmPassword" variant="outlined" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
-				<Button className="bg-orangePrimary" variant="contained" type='submit'>Reset Password</Button>
-			</form>
-		</div>
-	);
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (await resetPassword(email, password, confirmPassword, code) === true) {
+            router.push("/signin")
+        }
+    }
+
+    return (
+        <div className='flex flex-row items-center justify-center w-[400px]'>
+            <form className='flex flex-col space-y-4 w-full' onSubmit={handleSubmit}>
+                <TextField fullWidth placeholder="123456" focused required
+                           InputProps={{style: {fontStyle: 'italic', color: 'grey'},}} className="w-full" name='code'
+                           label="Code" variant="outlined" value={code} onChange={e => setCode(e.target.value)}/>
+                <TextField fullWidth placeholder="exemple@gmail.com" required
+                           InputProps={{style: {fontStyle: 'italic', color: 'grey'},}} className="w-full" name='email'
+                           label="Email" variant="outlined" value={email} onChange={e => setEmail(e.target.value)}/>
+                <TextField fullWidth placeholder="*******" required
+                           InputProps={{style: {fontStyle: 'italic', color: 'grey'},}} className="w-full"
+                           name='password' type='password' label="Password" variant="outlined" value={password}
+                           onChange={e => setPassword(e.target.value)}/>
+                <TextField fullWidth placeholder="*******" required
+                           InputProps={{style: {fontStyle: 'italic', color: 'grey'},}} className="w-full"
+                           name='confirmPassword' type='password' label="confirmPassword" variant="outlined"
+                           value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}/>
+                <Button className="bg-orangePrimary" variant="contained" type='submit'>Reset Password</Button>
+            </form>
+        </div>
+    );
 }
